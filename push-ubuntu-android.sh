da83feee7b7778b2c7bc4b02252257b26d1df88e
Andrew Chen <yongjhih@gmail.com>
2016-01-23 04:50:05 +0800

    Fix push and build

--- push-ubuntu-android.sh
+++ push-ubuntu-android.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in ubuntu-openjdk-8-android ubuntu-openjdk-8-android-extra ubuntu-openjdk-8-android-all; do
+for i in ubuntu-openjdk-8-android ubuntu-openjdk-8-android-extra ubuntu-openjdk-8-android-all ubuntu-openjdk-8-android-all-jenkins; do
     pushd $i
     docker push yongjhih/${i}:latest
     popd
