19e7db435c39aff468d3adc5ef95acb47d09cbb8
Andrew Chen <yongjhih@gmail.com>
2017-01-21 00:17:50 -0800

    Refine

--- 24/docker-entrypoint.sh
+++ 24/docker-entrypoint.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # Android SDK need to update usually except android-24 related
+
 mkdir "$ANDROID_HOME/licenses" || true
 echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
 echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
@@ -10,6 +11,5 @@ echo "y" | android update sdk -u -a --filter extra-android-m2repository && \
 echo "y" | android update sdk -u -a --filter extra-google-google_play_services && \
 echo "y" | android update sdk -u -a --filter extra-google-m2repository && \
 echo "y" | android update sdk -u -a --filter extra-google-analytics_sdk_v2
-./gradlew dependencies
 
 exec "$@"
